1f26c2297197e6dda4b3cc30008fc3f3457df9af
Pedro de Mattos <pedrodemattos86@gmail.com>
2019-09-12 19:15:28 -0300

    melhorando a performance do app fazendo com que se percorra menos o DOM

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,23 @@
 class NegociacaoController {
+
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+
+        this.inputData = $('#data');
+         this.inputQuantidade =  $('#quantidade');
+        this.inputValor = $('#valor'); 
+
+    }
     
     
     adiciona(event) {
         event.preventDefault();
         // para poder usar referencias a variaveis e permanecer se referindo ao document, e deixar um codigo mais limpo se usa o metodo bind. Se nao utiliza o js nao reconhece a variavel de atalho '$' porque ele nao identifica que se refere ao document.
-        let $ = document.querySelector.bind(document);
+        
 
-        let inputData = $('#data');
-        let inputQuantidade =  $('#quantidade');
-        let inputValor = $('#valor'); 
-
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
   }
 }
