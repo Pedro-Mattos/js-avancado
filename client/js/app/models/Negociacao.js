980946e7ebc4e2052fc4d3d94b164edc400e5936
Pedro de Mattos <pedrodemattos86@gmail.com>
2019-09-12 16:41:04 -0300

    utilizando programaçao defensiva com os objetos de negociacao

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,24 +2,29 @@ class Negociacao {
 
     //por convenção colocamos underline antes dos atributos que consideramos privados no js
     constructor(data, quantidade, valor){
-        this._data = data;
+
+        //programaçao defensiva guardando um objeto dentro de um objeto no caso de data que já é um objeto eu retorno um objeto getDate que é um numero representa a data dentro dele dificultando a alteraçao fora do ambiente interno
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+
+        Object.freeze(this);
     }
     // funçoes que sao desenvolvidas dentro de uma classe sao chamadas de metodo
-    getVolume () {
+    // vantagens do get é que vc pode chamar sem usar ()
+    get volume () {
        return this._quantidade * this._valor ; 
     }
 
-    getQuantidade () {
+    get quantidade () {
         return this._quantidade;
     }
-
-    getData () {
-        return this._data;
+    //forma de programaçao defensiva novamente, retronando um objeto dentro de outro no caso date é importante.
+    get data () {
+        return new Date(this._data.getTime());
     }
 
-    getValor() {
+    get valor() {
         return this._valor;
     }
 
