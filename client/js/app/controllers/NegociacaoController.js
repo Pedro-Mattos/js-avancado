31c0722bb3ab4c13fbde977f46830b37cbf8a695
Pedro de Mattos <pedrodemattos86@gmail.com>
2019-09-13 17:12:42 -0300

    atualizando a classe controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,22 +2,36 @@ class NegociacaoController {
 
 
     constructor(){
+         // para poder usar referencias a variaveis e permanecer se referindo ao document, e deixar um codigo mais limpo se usa o metodo bind. Se nao utiliza o js nao reconhece a variavel de atalho '$' porque ele nao identifica que se refere ao document.
         let $ = document.querySelector.bind(document);
 
-        this.inputData = $('#data');
-         this.inputQuantidade =  $('#quantidade');
-        this.inputValor = $('#valor'); 
+        this._inputData = $('#data');
+         this._inputQuantidade =  $('#quantidade');
+        this._inputValor = $('#valor'); 
 
     }
     
     
     adiciona(event) {
         event.preventDefault();
-        // para poder usar referencias a variaveis e permanecer se referindo ao document, e deixar um codigo mais limpo se usa o metodo bind. Se nao utiliza o js nao reconhece a variavel de atalho '$' porque ele nao identifica que se refere ao document.
-        
+       
+       //a função split é para partir a string da data recebida da seguinte maneira "ano-mes-dia" em um array com 3 strings, posteriormente a função map , mapeia os arrays e aplica uma funçao sobre o itens e indices, reduzindo em 1 o indice do mês, pois no retorno estava vindo errado com uma data a mais. 
 
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        let data = new Date(...this._inputData.value.split('-').map(function(item, indice) {
+                if(indice == 1) {
+                    return item - 1;
+                }
+                return item;
+            })
+        );
+        console.log(data);
+
+        //chama como parametro a data salva da forma correta na let declarada acima e os valores do input quantidade e valor
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+            )
   }
 }
