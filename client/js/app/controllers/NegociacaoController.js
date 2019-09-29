004bd125c864eb7d070472838e07dfa9f49b4a23
Pedro de Mattos <pedrodemattos86@gmail.com>
2019-09-29 20:13:17 -0300

    Utilizando Template String e criando list models

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,10 +4,10 @@ class NegociacaoController {
     constructor() {
         // para poder usar referencias a variaveis e permanecer se referindo ao document, e deixar um codigo mais limpo se usa o metodo bind. Se nao utiliza o js nao reconhece a variavel de atalho '$' porque ele nao identifica que se refere ao document.
         let $ = document.querySelector.bind(document);
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
@@ -16,14 +16,28 @@ class NegociacaoController {
 
         event.preventDefault();
     
-        let negociacao = new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
-    
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
     }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
+
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+              DateHelper.textoParaData(this._inputData.value),
+              this._inputQuantidade.value,
+              this._inputValor.value
+          );
     
 }
