e9de1ace8e832a192bb38884333b3fe628c9625b
Pedro de Mattos <pedrodemattos86@gmail.com>
2019-09-30 04:17:21 -0300

    usando reflect

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,9 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        
+        this._listaNegociacoes = new ListaNegociacoes(model => 
+            this._negociacoesView.update(model));
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -21,24 +23,20 @@ class NegociacaoController {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
         
         this._limpaFormulario();   
     }
-
-    apaga(){
-        this._listaNegociacoes.esvazia();
-        //atualiza a lista vazia na view
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = "Lista de negociações apagada com sucesso";
-        this.MensagemView.update(this._mensagem);
-
-    }
     
+    apaga() {
+        
+        this._listaNegociacoes.esvazia();
+         
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+    }
     _criaNegociacao() {
         
         return new Negociacao(
